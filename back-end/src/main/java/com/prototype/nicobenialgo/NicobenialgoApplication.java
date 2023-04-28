073d83347f1ad106596fa8575e4194f67110de96
Nicolas Federico Benialgo <nicolasbenialgo@hotmail.com>
2023-04-28 14:23:03 -0300

    Creación de estructura
    Se creó la base del back-end para el proyecto, mediante Springboot y Apache NetBeans

---
+++ back-end/src/main/java/com/prototype/nicobenialgo/NicobenialgoApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.nicobenialgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicobenialgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicobenialgoApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/prototype/nicobenialgo/NicobenialgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.nicobenialgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicobenialgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
